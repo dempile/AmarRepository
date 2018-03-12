7c24bb3ce98a6a2ca1178133ba7ab536911e2df1
cherchali.amar <dempile@hotmail.com>
2018-03-12 15:30:06 +0100

    message to commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { CoreModule } from './core/core.module';
+import { HomeModule } from './home/home.module';
+import { PagenotfoundModule } from './pagenotfound/pagenotfound.module';
+import { SharedModule } from './shared/shared.module';
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +14,11 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CoreModule,
+    SharedModule,
+    PagenotfoundModule,
+    HomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
